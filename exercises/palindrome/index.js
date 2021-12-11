4c8315413844d84ec434b8d085843f86cc2959b4
Kody Low <kodylow7@gmail.com>
2021-12-11 10:22:19 -0500

    solved palindrome

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -8,7 +8,13 @@
 //   palindrome("abcdefg") === false
 
 function palindrome(str) {
-  return str === str.split("").reverse().join("");
+  return str.split("").every((char, i) => {
+    return char === str[str.length - i - 1];
+  });
 }
 
 module.exports = palindrome;
+
+// function palindrome(str) {
+//   return str === str.split("").reverse().join("");
+// }
